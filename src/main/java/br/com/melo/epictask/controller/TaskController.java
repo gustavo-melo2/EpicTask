567e0b6bc4874621e6961812b7ef96002df2d32c
Gustavo Melo <gtvmelo10@gmail.com>
2021-10-30 13:29:13 -0300

    Dependencias DevTools e Thymeleaf adicionadas.

---
+++ src/main/java/br/com/melo/epictask/controller/TaskController.java
@@ -0,0 +1,17 @@
+package br.com.melo.epictask.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TaskController {
+
+	@RequestMapping("/task")
+	public String index() {
+		return "tasks";
+	}
+	
+	
+	
+	
+}
